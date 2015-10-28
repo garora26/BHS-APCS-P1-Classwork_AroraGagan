e9556b2e5de40571a6dcb7425da4ddf8e1e71e24
gaganarora <gaganarora@BELR90A97M0.SEA.CORP.EXPECN.com>
2015-10-27 21:02:42 -0700

    first commit

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -2,9 +2,25 @@ package HelloWorld;
 
 public class HelloWorld {
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+		// System.out.println(1 + 2.0 + "\"//\\\"" + 1 + 2.0);
+
+		int p = 1;
+		int pp = 1; 
+		
+		System.out.print("1 1 ");
+		for( int i = 1; i <= 10; i++)
+		{
+			int n = p + pp;
+			System.out.print(n + " " );
+			
+			pp = p;
+			p = n;
+		}
 	}
+	
 
 }
